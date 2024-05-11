85c88a26bc3283e2e12cda6d61677167c1c9d430
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-11 11:29:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,4 @@ var score = 0;
 document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = "Score: " + score; // Обновляем значение счетчика
-    
-    document.addEventListener("DOMContentLoaded", function() {
-    var tabs = document.querySelectorAll('.tab');
-
-    tabs.forEach(function(tab) {
-        tab.addEventListener('click', function(event) {
-            event.preventDefault(); // Предотвращаем переход по ссылке
-
-            var targetId = tab.getAttribute('data-target');
-            var popup = document.getElementById(targetId);
-            if (popup) {
-                popup.style.display = 'block';
-            }
-         });
-      });
-   });
 });
