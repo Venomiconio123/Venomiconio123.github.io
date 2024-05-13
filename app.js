c0253a444500ebbdab9723fec0e3ce1965f32732
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-14 00:53:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ var score = 0;
 document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = score; // Обновляем только значение числа
+    // Находим кнопку по id
+var button2 = document.getElementById("button2");
+
+// Добавляем обработчик события клика на кнопку
+button2.addEventListener("click", function() {
+    // Открываем новое окно при клике на кнопку
+    window.open("http://example.com", "_blank");
+});
+
 });
