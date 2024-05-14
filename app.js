ae32b4a46a96d23e72dcd126c50a7b63c04e331c
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-14 13:57:03 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,27 @@ document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = score; // Обновляем только значение числа
 // Находим все кнопки
+    // Находим все кнопки
+var tabs = document.querySelectorAll('.tab');
+
+// Добавляем обработчик события клика для каждой кнопки
+tabs.forEach(function(tab) {
+    tab.addEventListener('click', function() {
+        // Получаем значение атрибута data-target
+        var targetId = tab.getAttribute('data-target');
+        
+        // Находим элемент с соответствующим id
+        var targetPopup = document.getElementById(targetId);
+        
+        // Скрываем все элементы с классом popup
+        var popups = document.querySelectorAll('.popup');
+        popups.forEach(function(popup) {
+            popup.style.display = 'none';
+        });
+        
+        // Отображаем выбранный элемент
+        targetPopup.style.display = 'block';
+    });
+});
+
 });
