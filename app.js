0650a4cdb6845df6d4db59180c2307bbffba5b18
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-14 18:09:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,5 @@ var score = 0;
 document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = score; // Обновляем только значение числа
-// Находим все кнопки
-    // Находим все кнопки
-var tabs = document.querySelectorAll('.tab');
 
 });
