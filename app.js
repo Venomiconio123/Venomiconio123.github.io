76543de9962823b97e7b6b2f091ba935891dca10
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-14 12:59:15 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,23 +4,4 @@ document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = score; // Обновляем только значение числа
 // Находим все кнопки
-var buttons = document.querySelectorAll(".tab");
-
-// Находим контейнер с "страницами"
-var popupContainer = document.getElementById("popup-container");
-
-// Добавляем обработчик события клика для каждой кнопки
-buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
-        // Получаем значение атрибута data-target кнопки
-        var target = button.getAttribute("data-target");
-        
-        // Находим "страницу" с таким же id и показываем её
-        var popup = document.getElementById(target);
-        if (popup) {
-            popupContainer.style.display = "block";
-            popup.style.display = "block";
-        }
-    });
-});
 });
