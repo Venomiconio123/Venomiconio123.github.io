a2b116a9aec3f69081613ab582c8f2fa8fdecd77
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-14 18:36:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -10,21 +10,29 @@ document.querySelectorAll('.tab').forEach(function(tab) {
         var imageUrl = ''; // Установите соответствующий URL изображения для каждой кнопки
         switch(target) {
             case 'menu1':
-                imageUrl = '2.png';
+                imageUrl = 'image1.png';
                 break;
             case 'menu2':
-                imageUrl = '2.png';
+                imageUrl = 'image2.png';
                 break;
             case 'menu3':
-                imageUrl = '2.png';
+                imageUrl = 'image3.png';
                 break;
             case 'menu4':
-                imageUrl = '2.png';
+                imageUrl = 'image4.png';
                 break;
             default:
                 imageUrl = '';
         }
-        document.getElementById('image-container').innerHTML = '<img src="' + imageUrl + '" alt="Image">';
+        var imageContainer = document.getElementById('image-container');
+        if (imageUrl !== '') {
+            imageContainer.innerHTML = '<img src="' + imageUrl + '" alt="Image">';
+            imageContainer.style.display = 'block'; // Показываем контейнер с изображением
+        } else {
+            imageContainer.innerHTML = ''; // Очищаем контейнер, если изображение не найдено
+            imageContainer.style.display = 'none'; // Скрываем контейнер, если изображение не найдено
+        }
     });
-  });
+});
+
 });
