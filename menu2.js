0f1e84de9411a8ad9912dd09f41f689a590044f1
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-16 14:53:37 +0700

    Update menu2.js

--- menu2.js
+++ menu2.js
@@ -1,11 +1,3 @@
-var score = 0;
-
-document.getElementById("btn").addEventListener("click", function() {
-    score++;
-    document.getElementById("score").innerText = score; // Обновляем только значение числа
-});
-
-// Переносим этот блок кода изнутри обработчика события клика на кнопке
 document.querySelectorAll(".tab").forEach(function(tab) {
     tab.addEventListener("click", function() {
         var target = this.getAttribute("data-target");
