ead420b2031cc78d544669367c6884c20c08cd93
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-16 12:43:34 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = score; // Обновляем только значение числа
 
-    document.getElementById("btn").addEventListener("click", function() {
+    document.getElementById("tab").addEventListener("click", function() {
     // Перенаправление на новую страницу
     window.location.href = "menu1.html";
   });
