55131d403c6d026283495b656a744ade7a0dea83
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-16 20:33:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ document.getElementById("btn").addEventListener("click", function() {
     document.getElementById("score").innerText = score; // Обновляем только значение числа
 });
 
+var score = 0;
+var maxClicks = 10; // Максимальное количество нажатий
+
+document.getElementById("btn").addEventListener("click", function() {
+    if (score < maxClicks) {
+        score++;
+        document.getElementById("score").innerText = score; // Обновляем только значение числа
+    } else {
+        alert("Вы достигли максимального количества нажатий.");
+    }
+});
+
 // Переносим этот блок кода изнутри обработчика события клика на кнопке
 document.querySelectorAll(".tab").forEach(function(tab) {
     tab.addEventListener("click", function() {
@@ -12,3 +24,4 @@ document.querySelectorAll(".tab").forEach(function(tab) {
         window.location.href = target + ".html"; // Перенаправление на страницу с соответствующим именем
     });
 });
+
