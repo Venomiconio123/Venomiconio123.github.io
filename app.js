575ea4419cde99999bb26daf8407db8dc4ccb5fe
Venomiconio123 <bandaletov0822@bk.ru>
2024-05-16 14:46:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var score = 0;
 document.getElementById("btn").addEventListener("click", function() {
     score++;
     document.getElementById("score").innerText = score; // Обновляем только значение числа
-
+});
 
 // Переносим этот блок кода изнутри обработчика события клика на кнопке
 document.querySelectorAll(".tab").forEach(function(tab) {
@@ -11,5 +11,4 @@ document.querySelectorAll(".tab").forEach(function(tab) {
         var target = this.getAttribute("data-target");
         window.location.href = target + ".html"; // Перенаправление на страницу с соответствующим именем
     });
-  });
 });
